dedc4c247a37a28b9e12d0121238dfa916401375
halimzidoune <halimzidoune@gmail.com>
2022-04-28 19:00:40 +0100

    first commit

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <div>
-      <router-view></router-view>
-    </div>
-  </div>
+    <v-app app>
+        <v-container>
+            <v-btn color="primary">
+                Primary
+            </v-btn>
+            <v-btn color="secondary">
+                Secondary
+            </v-btn>
+            <v-btn color="error">
+                Error
+            </v-btn>
+        </v-container>
+    </v-app>
+
 </template>
 <script>
 export default {};
